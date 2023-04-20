5ebafa2b680632b25ca8ce61d3d50ddf4c862c28
hikmatillayev <hikmatillayevoktam24@gmail.com>
2023-04-20 13:37:21 +0500

    slice  card page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import { Routes, Route } from "react-router-dom"
-import {Main, Login, Register, Navbar} from './components'
+import { Main, Login, Register, Navbar } from './components'
 import AuthService from "./service/auth"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { signUserSuccess } from "./slice/auth"
 import { getItem } from "./helpers/persistance-storage"
+import ArticleService from "./service/article"
+import { getArticleSucces, getArticlesStart } from "./slice/article"
 
 const App = () => {
-  const dispatch=useDispatch()
+  const dispatch = useDispatch()
 
-  const getUser=async()=>{
-    try{
-      const response=await AuthService.getUser()
+  const getUser = async () => {
+    try {
+      const response = await AuthService.getUser()
       dispatch(signUserSuccess(response.user))
-    }catch (error) {}
+    } catch (error) { }
   }
 
-  useEffect(()=>{
-    const token=getItem('token')
+  const getArticles = async () => {
+    dispatch(getArticlesStart())
+    try {
+      const response = await ArticleService.getArticles()
+      dispatch(getArticleSucces(response.articles))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    const token = getItem('token')
     if (token) {
       getUser()
     }
+    getArticles()
   }, [])
 
   return (
     <div>
-    <Navbar/> 
-    <Routes>
-      <Route path="/" element={<Main/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-    </Routes>
- </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   )
 }
 
