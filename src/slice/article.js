1770801fcb38b00f63f5e5c4764fa19ac887ff92
hikmatillayev <hikmatillayevoktam24@gmail.com>
2023-04-20 15:01:06 +0500

    Detail article

--- src/slice/article.js
+++ src/slice/article.js
@@ -1,27 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-    isLoading:false,
-    articles:[],
-    error:null,
+const initialState = {
+    isLoading: false,
+    articles: [],
+    error: null,
+    articleDetail: null,
 }
 
-export const articleSlice=createSlice({
-    name:'article',
+export const articleSlice = createSlice({
+    name: 'article',
     initialState,
-    reducers:{
-        getArticlesStart:state=>{
-            state.isLoading=true
+    reducers: {
+        getArticlesStart: state => {
+            state.isLoading = true
         },
-        getArticleSucces:(state, action)=>{
-            state.isLoading=false
-            state.articles=action.payload
+        getArticleSucces: (state, action) => {
+            state.isLoading = false
+            state.articles = action.payload
         },
-        getArticleFailure:(state, action)=>{
-            state.error=action.payload
+        getArticleFailure: (state, action) => {
+            state.error = action.payload
+            state.isLoading = false
+        },
+        getArticleDetailStart: state => {
+            state.isLoading = true
+        },
+        getArticleDetailSuccess: (state, action) => {
+            state.isLoading = false
+            state.articleDetail = action.payload
+        },
+        getArticleDetailFailure: state => {
+            state.isLoading = false
         },
     },
 })
 
-export const {getArticleSucces, getArticlesStart, getArticleFailure}=articleSlice.actions
+export const { getArticleSucces, getArticlesStart, getArticleFailure, getArticleDetailStart, getArticleDetailSuccess, getArticleDetailFailure } = articleSlice.actions
 export default articleSlice.reducer
