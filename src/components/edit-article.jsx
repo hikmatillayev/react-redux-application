7d25da1babd2eef40ca52a36da94856deeb33b9b
hikmatillayev <hikmatillayevoktam24@gmail.com>
2023-05-01 18:37:35 +0500

    article edit api

--- src/components/edit-article.jsx
+++ src/components/edit-article.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
-import { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess } from "../slice/article"
+import { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess, postArticleFailure, postArticleStart, postArticleSuccess } from "../slice/article"
 import ArticleService from "../service/article"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import ArticleForm from "./article-form"
 
 const EditArticle = () => {
@@ -11,6 +11,7 @@ const EditArticle = () => {
     const [body, setBody] = useState('')
     const dispatch = useDispatch()
     const { slug } = useParams()
+    const navigate=useNavigate()
 
     useEffect(() => {
         const getArticleDetail = async () => {
@@ -29,7 +30,18 @@ const EditArticle = () => {
         getArticleDetail()
     }, [])
 
-    const formSubmit=()=>{}
+    const formSubmit = async e => {
+        e.preventDefault()
+        const article = { title, description, body }
+        dispatch(postArticleStart())
+        try {
+            await ArticleService.editArticle(slug, article)
+            dispatch(postArticleSuccess())
+            navigate('/')
+        } catch (error) {
+            dispatch(postArticleFailure())
+        }
+    }
 
     const formProps = { title, setTitle, description, setDescription, body, setBody, formSubmit }
 
@@ -38,7 +50,7 @@ const EditArticle = () => {
         <div className="text-center">
             <h1 className="fs-2">EditArticle</h1>
             <div className="w-75 mx-auto">
-                <ArticleForm {...formProps}/>
+                <ArticleForm {...formProps} />
             </div>
         </div>
     )
